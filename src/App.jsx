68b6d0beb698a0c6b5eb422b90e9b2255a90c536
Raquel Souto <raquelsouto@MacBook-Pro-de-Raquel.local>
2024-10-31 20:02:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./style.css";
+
+function App() {
+  return (
+    <div className="texto-prueba">
+      <p>Menos Cachitas y Más Cucharitas</p>
+    </div>
+  );
+}
+
+export default App;
