721194f521ab04b6eead681fb4a1b0aac812120a
Raquel Souto <raquelsouto@MacBook-Air-de-Raquel.local>
2024-12-24 11:10:52 +0100

    Ajustes vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    host: '0.0.0.0', 
+    port: 3000,      
   },
   build: {
     outDir: 'dist',
   },
 })
+
+
+
 
